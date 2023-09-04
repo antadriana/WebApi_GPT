50fa2c643d4f9f2873f660a684045fd239de91c6
adriana-antoniuk-swi <adriana.antoniuk@solarwinds.com>
2023-09-04 20:02:01 +0300

    UC#1 Integration

--- WebApi_GPT/Controllers/CountriesController.cs
+++ WebApi_GPT/Controllers/CountriesController.cs
@@ -17,20 +17,42 @@ public class CountriesController : ControllerBase
 
     [HttpGet]
     public async Task<IActionResult> GetCountries(
-        string param1 = "",
-        int param2 = 0,
-        string param3 = "",
-        string param4 = "")
+     string nameFilter = "",
+     int populationFilterInMillions = 0,
+     string sortOrder = "ascend",
+     int limit = 50) // default limit set to 50
     {
         var client = _clientFactory.CreateClient();
-
         HttpResponseMessage response = await client.GetAsync("https://restcountries.com/v3.1/all");
 
         if (response.IsSuccessStatusCode)
         {
             var jsonString = await response.Content.ReadAsStringAsync();
-
             var countries = JsonSerializer.Deserialize<List<CountryDto>>(jsonString);
+
+            // Filter countries by name
+            if (!string.IsNullOrWhiteSpace(nameFilter))
+            {
+                countries = FilterCountriesByName(countries, nameFilter);
+            }
+
+            // Filter countries by population
+            if (populationFilterInMillions > 0)
+            {
+                countries = FilterCountriesByPopulation(countries, populationFilterInMillions);
+            }
+
+            // Sort countries by name
+            if (!string.IsNullOrEmpty(sortOrder))
+            {
+                countries = SortCountriesByName(countries, sortOrder);
+            }
+
+            // Limit number of records
+            if (limit > 0)
+            {
+                countries = LimitNumberOfRecords(countries, limit);
+            }
 
             return Ok(countries);
         }
@@ -40,7 +62,7 @@ public class CountriesController : ControllerBase
         }
     }
 
-    public static List<CountryDto> FilterCountriesByName(List<CountryDto> countries, string filter)
+public static List<CountryDto> FilterCountriesByName(List<CountryDto> countries, string filter)
     {
         return countries.Where(c =>
             (c.name?.common != null && c.name.common.IndexOf(filter, StringComparison.OrdinalIgnoreCase) >= 0) ||
